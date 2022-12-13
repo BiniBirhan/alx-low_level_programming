d3ea46995fbb421f79292ec7db3f46ba8cca11f7
BiniBirhan <biniamabera@wcu.edu.et>
2022-12-13 01:56:57 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include"maun.h"
 
 /**
  *main - Entry point
@@ -8,7 +8,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char put[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(put[i])
+	}
+	_putchar('\n');
 	return (0);
 }
 
