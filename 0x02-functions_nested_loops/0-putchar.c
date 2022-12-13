817221c94e98ade4180ba4882fae86c93cce3384
BiniBirhan <biniamabera@wcu.edu.et>
2022-12-13 01:33:56 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *Description: program to print_putchar
+ * Return: 0 Always (Success
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
+
