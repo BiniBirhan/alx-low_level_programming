33be8e7b294964f8c00e73bc796b0c6765e5a77c
BiniBirhan <biniamabera@wcu.edu.et>
2022-12-12 17:54:43 -0800

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,6 +11,7 @@
 */
 
 int main(void)
+
 {
 	int n;
 
@@ -18,15 +19,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d:is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d:is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d:is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
